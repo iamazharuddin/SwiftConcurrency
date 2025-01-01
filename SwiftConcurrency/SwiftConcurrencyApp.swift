c754691eddd34aa400ac866f4c0e3486705f1669
Azharuddin <iamazharuddin10@gmail.com>
2025-01-01 11:43:13 +0530

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Azharuddin 1 on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
